deaf987489f5c025dc33566acf7962e66d859d58
Eric Martinez <eric.michael.mtz@gmail.com>
2020-07-22 17:11:13 -0500

    update for codio

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require "sinatra"
 
-#makes this web application available outside of this computer
-set :bind, '0.0.0.0'
+require 'sinatra'
 
-get "/hello" do
-    erb :hello_form
+class MyApp < Sinatra::Base
+    get "/hello" do
+        erb :hello_form
+    end
+    
+    post "/hello" do
+        @name = params["name"]
+        erb :hello
+    end
 end
 
-post "/hello" do
-    @name = params["name"]
-    erb :hello
-end
 
 
 
